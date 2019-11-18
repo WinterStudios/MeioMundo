a98dc41282fb4c1cc14b94c91c4ca8a4e398d205
Hugo Marques <hugo_filipe_marques@live.com.pt>
2019-11-18 22:40:32 +0000

    v1d

--- MeioMundo/MeioMundoWPF/MainWindow.xaml.cs
+++ MeioMundo/MeioMundoWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tools;
 
 namespace MeioMundoWPF
 {
@@ -24,13 +26,34 @@ namespace MeioMundoWPF
         public MainWindow()
         {
             InitializeComponent();
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            var asm = Assembly.LoadFile(System.IO.Directory.GetCurrentDirectory() + "/Tools.dll");
+            var types = asm.GetTypes().Where(x => x.IsSubclassOf(typeof(UserControl)));
+            foreach (var item in types)
+            {
+                MenuItem i = new MenuItem();
+                var menuItem = new MenuItem();
+                menuItem.Click += (sender, e) =>
+                {
+                    object tool = Activator.CreateInstance(item);
+                    Panel.Children.Add(tool as UserControl);
+                };
+                menuItem.Header = item.Name;
+                MenuItemTools.Items.Add(menuItem);
+            }            
         }
 
         private void Close_Application(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
+        private void MaximazeWindowEvent(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
