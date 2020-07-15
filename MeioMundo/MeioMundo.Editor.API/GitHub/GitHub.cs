3ab15859fb069383ce967e9c45950d192163f017
Hugo Marques <hugo_filipe_marques@live.com.pt>
2020-07-15 16:40:39 +0100

    1212

---
+++ MeioMundo/MeioMundo.Editor.API/GitHub/GitHub.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeioMundo.Editor.API.GitHub
+{
+    public class GitHub
+    {
+        public static string User { get { return "WinterStudios"; } }
+        public static string Repository { get { return "MeioMundo"; } }
+        public static string GitHubUrl { get { return "https://api.github.com/repos/"; } }
+        /// <summary>
+        /// Get the Last Version of the Reposity
+        /// </summary>
+        public static void GetRelease()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/WinterStudios/MeioMundo/releases/latest");
+            webRequest.Method = "GET";
+            webRequest.UserAgent = "Anything";
+            webRequest.ServicePoint.Expect100Continue = false;
+
+            WebResponse response = webRequest.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            Console.WriteLine(reader.ReadToEnd());
+        }
+        /// <summary>
+        /// Get the Last Version of the Reposity
+        /// </summary>
+        /// <param name="user">Username</param>
+        /// <param name="reposity">Reposity Name</param>
+        public static void GetRelease(string user, string reposity)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/"+ user +"/" + reposity + "/releases/latest");
+            webRequest.Method = "GET";
+            webRequest.UserAgent = "Anything";
+            webRequest.ServicePoint.Expect100Continue = false;
+
+            WebResponse response = webRequest.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
---
+++ MeioMundo/MeioMundo.Editor.API/GitHub/Release.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeioMundo.Editor.API.GitHub
+{
+    public class Release
+    {
+        public string Url { get; set; }
+        public string Tag { get; set; }
+        public bool PreRelease { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Asset[] Assets { get; set; }
+    }
+
+    public struct Asset
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string DownloadLink { get; set; }
+    }
+}
